42ebae12ff6d28426271635d2243699014991237
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-12 16:22:52 +0530

    added

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -1,22 +1,36 @@
 import java.util.Scanner;
 
 interface Resume {
-    // TODO: Declare method void biodata();
+    void biodata();
 }
 
 class Teacher implements Resume {
-    // Attributes
-   
+    String name;
+    String qualification;
+    int experience;
 
-    // TODO: Implement biodata() method to print details in the required format
+    Teacher(String name, String qualification, int experience) {
+        this.name = name;
+        this.qualification = qualification;
+        this.experience = experience;
+    }
+
+    public void biodata() {
+        System.out.println("Name: " + name);
+        System.out.println("Qualification: " + qualification);
+        System.out.println("Experience: " + experience + " years");
+    }
 }
 
 public class InterfaceResume {
-   
-        
-        // TODO: Read input (Name, Qualification, Experience)
-        // Assign to teacher object attributes
-        
-        // TODO: Call teacher.biodata()
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        String qualification = sc.nextLine();
+        int experience = sc.nextInt();
+
+        Teacher t = new Teacher(name, qualification, experience);
+        t.biodata();
+    }
 }
